de74291418328198c9b913fa1aec7bf9f9102f56
t-l-xin <e0556433@u.nus.edu>
2021-09-10 17:21:45 +0800

    Update circus and animal classes

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,11 +1,13 @@
 package circus;
 
-import circus.animal.Animal;
-import circus.animal.Duck;
-import circus.animal.Parrot;
+import circus.animal.*;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -39,8 +41,40 @@ public class Circus {
     }
 
     public static void main(String[] args) {
+        System.out.println("Num of animals: " + animals.length);
+        /**
         makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
         System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        */
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+        for(Animal a: animalArrayList){
+            System.out.println(a);
+        }
+
+        System.out.println("Size of array list: " + animalArrayList.size());
+        animalArrayList.add(new Parrot("Dolly"));
+        animalArrayList.add(new Duck("Goose"));
+
+        Duck louie = new Duck("louie");
+        animalArrayList.add(louie);
+
+        System.out.println("Size of array list: " + animalArrayList.size());
+
+        Elephant strongOne = new Elephant("Strong One");
+        Tiger sharekhan = new Tiger("Share Khan");
+        printAllAnimals(animalArrayList);
+        System.out.println("Index of array list: " + animalArrayList.indexOf(louie));
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+        System.out.println("After sorting");
+        printAllAnimals(animalArrayList);
+
+    }
+
+    private static void printAllAnimals(ArrayList<Animal> animalArrayList){
+        for(Animal a: animalArrayList){
+            System.out.println(a);
+        }
     }
 }
